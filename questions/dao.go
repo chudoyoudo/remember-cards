c9e052addbbad8e8ff4d979552999df04d0cecfe
Leonid Turchenkov <turchenkoff@gmail.com>
2021-02-14 01:53:57 +0700

    question usecase tests fixes

--- questions/dao.go
+++ questions/dao.go
@@ -2,7 +2,4 @@ package questions
 
 type Dao interface {
     Create(q *Question) error
-    Update(q *Question) error
-    Delete(conds ...interface{}) error
-    Find(limit, offset int, conds ...interface{}) (list *[]Question, more bool, err error)
 }
